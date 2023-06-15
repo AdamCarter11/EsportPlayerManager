b1f67117f82da462ece95dfa8d7f23e7e9a5a8c7
Adam <57573881+AdamCarter11@users.noreply.github.com>
2023-06-15 15:50:40 -0700

    Starting to rework inheritance

---
+++ EsportPlayerManager/Assets/Scripts/ReworkingInheritance/BaseCharacter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour
+{
+    public void ApplyClassBonuses()
+    {
+
+    }
+    private int CountOccupationalClass(OccupationClassType elementCheck)
+    {
+        return -1;
+    }
+    private int CountElementClass(ElementalClassType elementCheck)
+    {
+        return -1;
+    }
+    public void ActivateAbility()
+    {
+
+    }
+    public void changeStats(CharacterClass charClassRef)
+    {
+
+    }
+    public void TakeDamage(int damageToTake)
+    {
+
+    }
+    public void swapCharacters()    // this function also calls the ienumerator for attack, so it probably needs an extra parameter
+    {
+
+    }
+    public void AttackTrigger()
+    {
+        
+    }
+    //maybe also need a stop attacking func
+}
---
+++ EsportPlayerManager/Assets/Scripts/ReworkingInheritance/PlayerV2.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerV2 : BaseCharacter
+{
+    // references
+    [SerializeField] CharacterClass charClass;
+    private MainEnemy enemyRef;
+    BanPickUI banPickRef;
+
+    // checks
+    bool startCombat = false;
+    bool resetHealth = false;
+
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "CombatScene")
+        {
+            enemyRef = GameObject.FindGameObjectWithTag("MainEnemy").GetComponent<MainEnemy>();
+            banPickRef = GameObject.FindGameObjectWithTag("CombatUI").GetComponent<BanPickUI>();
+        }
+    }
+    private void Update()
+    {
+        if (startCombat)
+        {
+            print("Starting combat");
+            startCombat = false;
+            resetHealth = true;
+            enemyRef.SwitchEnemyToAttack();
+            StartCoroutine(CoroutineAttack());
+        }
+        if (charClass.tempHealth <= 0 && resetHealth)
+        {
+            print("character death");
+            swapCharacters();
+        }
+    }
+    IEnumerator CoroutineAttack()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(charClass.baseAttackSpeed);
+            AttackTrigger();
+        }
+    }
+
+}
